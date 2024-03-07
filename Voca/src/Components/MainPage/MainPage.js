aafb860e0421154cc2dcc0f7951dcc68053c64e5
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-03-07 05:48:40 -0500

    adding a get request

--- Voca/src/Components/MainPage/MainPage.js
+++ Voca/src/Components/MainPage/MainPage.js
@@ -45,7 +45,7 @@ const MainPage = ({ userData }) => {
         await axios.post("http://localhost:5000/messages/send", {
           text: message,
         });
-        await axios.post("http://localhost:5000/messages", { text: message });
+        await axios.get("http://localhost:5000/messages", { text: message });
       } catch (error) {
         console.error("Error sending message to the backend:", error);
       }
