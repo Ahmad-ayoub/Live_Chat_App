e403ab2e95e18468918277650087c816539f7bb3
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-03-08 06:17:25 -0500

    switching back the sociekt io

--- Voca/src/Components/MainPage/MainPage.js
+++ Voca/src/Components/MainPage/MainPage.js
@@ -32,6 +32,7 @@ const MainPage = ({ userData }) => {
   const { fontSize } = useContext(FontContext);
   const currentFontClasses =
     FontClasses[fontSize] || FontClasses["fontDefault"];
+  const socket = io("http://localhost:3000");
   const [message, setMessage] = useState([]);
   const [chat, setChat] = useState([]);
 
@@ -54,7 +55,6 @@ const MainPage = ({ userData }) => {
   };
 
   useEffect(() => {
-    const socket = io("http://localhost:3000");
     socket.on("chat message", (msg) => {
       setChat((prevChat) => [
         ...prevChat,
