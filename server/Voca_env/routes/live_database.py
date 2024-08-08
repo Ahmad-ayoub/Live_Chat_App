375a8a2cfa3ded2bae21b40e98b22c7c3e9a00da
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-08-07 20:26:00 -0400

    adding table in psycopg2 file

--- server/Voca_env/routes/live_database.py
+++ server/Voca_env/routes/live_database.py
@@ -4,4 +4,9 @@ conn = psycopg2.connect("dbname=users_database_xz8x user=users_database_xz8x_use
 
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE userdata()")
+cur.execute(
+    "CREATE TABLE userdata(id integer PRIMARY KEY, name str, email str, username str, password str, birthdate date);"
+)
+cur.execute(
+    "CREATE TABLE messages(id integer PRIMARY KEY, user_id integer, text text, timestamp datetime, group_room_number str, user );"
+)
