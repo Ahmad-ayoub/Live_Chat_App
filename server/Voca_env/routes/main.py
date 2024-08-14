96f38945c08cefb41a5a5cac015992259ebf5599
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-08-14 11:19:51 -0400

    adding init.py file

--- server/Voca_env/routes/main.py
+++ server/Voca_env/routes/main.py
@@ -10,7 +10,9 @@ import logging
 from datetime import datetime
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS, cross_origin
-from server.Voca_env.routes.token_keys import (
+
+__all__ = ["token_keys"]
+from token_keys.token_keys import (
     login_key,
     user_id_key,
     group_id_key,
