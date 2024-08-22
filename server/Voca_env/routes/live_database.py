69c05697e24fb7c8493540e03ec6bee85a15dc6e
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-08-22 19:50:07 -0400

    fixing up psycopg2 connection

--- server/Voca_env/routes/live_database.py
+++ server/Voca_env/routes/live_database.py
@@ -12,42 +12,17 @@ try:
 
     cur = conn.cursor()
 
-    print("PostgreSQL server information")
-    print(conn.get_dsn_parameters(), "\n")
+    cur.execute(
+        """SELECT userdata FROM information_schema.tables WHERE table_schema = 'public'"""
+    )
+    for table in cur.fetchall():
+        print(table)
 
-    cur.execute("SELECT version();")
-
-    record = cur.fetchone()
-    print("You are connected to - ", record, "\n")
-
-    # cur.execute(
-    #     """CREATE TABLE userdata (
-    #             id SERIAL PRIMARY KEY, name varchar, email varchar, username varchar, password varchar, birthdate date
-    #             )
-
-    #  """
-    # )
-
-    # cur.execute(
-    #     """INSERT INTO userdata (id, name, email, username, password, birthdate)
-    #                 VALUES (%(int)s, %(str)s, %(str)s, %(str)s, %(str)s, %(date)s)
-
-    #              """
-    # )
-
-    # cur.execute(
-    #     """INSERT INTO messages (id, user_id, text, timestamp, group_room_number, user)
-    #         VALUES (%(int)s, %(int)s, %(text)s, %(date)s, %(str)s, %(str)s)
-
-    # """
-    # )
-
-    # cur.execute(
-    #     """ CREATE TABLE messages(
-    #             id integer primary key, user_id integer , text text, timestamp date, group_room_number varchar, user)
-
-    # """
-    # )
+    cur.execute(
+        """SELECT messages FROM information_schema.tables WHERE table_schema = 'public'"""
+    )
+    for table in cur.fetchall():
+        print(table)
 
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
