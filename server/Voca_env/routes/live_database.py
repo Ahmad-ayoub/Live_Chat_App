1d90d7df1dced8142db68c332a2993dc5b46d707
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-08-24 14:23:56 -0400

    using different method

--- server/Voca_env/routes/live_database.py
+++ server/Voca_env/routes/live_database.py
@@ -1,34 +1,29 @@
 import psycopg2
-from psycopg2 import Error
 
-try:
-    conn = psycopg2.connect(
-        host="dpg-cqmn9c5svqrc73fd29ng-a",
-        dbname="users_database_xz8x",
-        user="users_database_xz8x_user",
-        password="otApHYv9tr6smNmsMYaTjza9qD9sFEVT",
-        port="postgresql://users_database_xz8x_user:otApHYv9tr6smNmsMYaTjza9qD9sFEVT@dpg-cqmn9c5svqrc73fd29ng-a.ohio-postgres.render.com/users_database_xz8x",
-    )
+# conn = psycopg2.connect(
+#     host="dpg-cqmn9c5svqrc73fd29ng-a",
+#     dbname="users_database_xz8x",
+#     user="users_database_xz8x_user",
+#     password="otApHYv9tr6smNmsMYaTjza9qD9sFEVT",
+#     port="postgresql://users_database_xz8x_user:otApHYv9tr6smNmsMYaTjza9qD9sFEVT@dpg-cqmn9c5svqrc73fd29ng-a.ohio-postgres.render.com/users_database_xz8x",
+# )
 
-    cur = conn.cursor()
 
-    cur.execute(
-        """SELECT userdata FROM information_schema.tables WHERE table_schema = 'public'"""
-    )
-    for table in cur.fetchall():
-        print(table)
+def connect_to_database():
 
-    cur.execute(
-        """SELECT messages FROM information_schema.tables WHERE table_schema = 'public'"""
-    )
-    for table in cur.fetchall():
-        print(table)
+    database_url = "postgresql://users_database_xz8x_user:otApHYv9tr6smNmsMYaTjza9qD9sFEVT@dpg-cqmn9c5svqrc73fd29ng-a.ohio-postgres.render.com/users_database_xz8x"
+    user = "users_database_xz8x_user"
+    password = "otApHYv9tr6smNmsMYaTjza9qD9sFEVT"
 
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-finally:
-    if conn:
-        conn.commit()
-        cur.close()
-        conn.close()
-        print("PostgreSQL connection is closed")
+    connection = psycopg2.connect(database_url, user=user, password=password)
+    return connection
+
+
+def close_connection(connection):
+    """Closes a psycopg2 connection.
+
+    Args:
+        connection: A psycopg2 connection object.
+    """
+
+    connection.close()
