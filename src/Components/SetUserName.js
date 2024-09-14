3a96b775a8619695b23dc1a0d705e13ed78c8b1c
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-09-14 15:16:26 -0400

    utlize useRef

--- src/Components/SetUserName.js
+++ src/Components/SetUserName.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext, useContext } from "react";
+import { useEffect, useState, createContext, useContext, useRef } from "react";
 
 export const userNameContext = createContext();
 
@@ -6,17 +6,18 @@ export const SetUserData = () => useContext(userNameContext);
 
 export const UserNameProvider = ({ userData, children }) => {
   const [userName, setUserName] = useState();
+  const current_userName = useRef("");
 
   useEffect(() => {
     localStorage.setItem("current_username", userData.username);
   });
 
   useEffect(() => {
-    current_userName = "";
     current_userName = localStorage.getItem(
       userData.username,
       "current_username"
     );
+    current_userName.current = "current_username";
     if (current_userName) {
       setUserName(current_userName);
     }
