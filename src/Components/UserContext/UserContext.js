5bffe20cbed6371ae64d55cff6f771caf1bfdbc0
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-09-24 22:17:42 -0400

    adjusting userContext code

--- src/Components/UserContext/UserContext.js
+++ src/Components/UserContext/UserContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(
+    JSON.parse(localStorage.getItem("userData")) || null
+  );
 
   const updateUserData = (newUserData) => {
     localStorage.setItem("userData", JSON.stringify(newUserData));
