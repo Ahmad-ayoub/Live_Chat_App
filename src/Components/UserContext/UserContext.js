6348c85b93bbec32a562bd69b30b075592fc8c66
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-09-27 19:36:36 -0400

    importing axios

--- src/Components/UserContext/UserContext.js
+++ src/Components/UserContext/UserContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState, axios } from "react";
+import React, { createContext, useState } from "react";
+import axios from "axios";
 
 export const UserContext = createContext();
 
