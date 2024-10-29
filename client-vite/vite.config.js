09b94021ca1e31a97c527cfde01b08952fd2527f
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-10-28 22:01:21 -0400

    adjusting local host

--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:44305",
+        target: "https://localhost:5000",
         changeOrigin: true,
         secure: false,
         ws: true,
