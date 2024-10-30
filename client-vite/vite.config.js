049444cd2961b01518732938a447ced0ebe0c16a
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-10-30 19:34:09 -0400

    added in commonJS

--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import commonjs from "vite-plugin-commonjs";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs()],
+  plugins: [react(), commonjs()],
   build: {
     sourcemap: true,
   },
