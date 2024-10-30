af632b550b0e0fe2d6d7282270df299af6379df7
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-10-30 19:46:48 -0400

    fixing link for testing

--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import commonjs from "vite-plugin-commonjs";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
   build: {
